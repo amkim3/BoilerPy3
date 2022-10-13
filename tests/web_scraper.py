3ae0da39d907c3d3b17be740915c385172432a6a
amkim3 <amkim3@crimson.ua.edu>
2022-10-12 21:46:54 -0500

    Updated web scraper to loop through file that contains URLs. if it fails due to too many https requests, it tries again in 5 seconds with a max of 4 tries.

--- tests/web_scraper.py
+++ tests/web_scraper.py
@@ -1,7 +1,26 @@
+from datetime import time
+
 from boilerpy3 import extractors
+import time
 
 extractor = extractors.ArticleExtractor()
-
+count = 1
 # From a URL
-content = extractor.get_content_from_url('https://www.westalabamaaging.org/benefits-services')
-print(content)
+with open("extractedText6.txt", "a") as file:
+    with open("updatedLinks.txt") as urls:
+        for url in urls:
+            extracted = None
+            file.write(url + '\n')
+            for x in range(0,4):
+                try:
+                    file.write(extractor.get_content_from_url(url))
+                    print(url)
+                    error = None
+                except Exception as e:
+                    error = str(e)
+                if error:
+                    time.sleep(5)
+                else:
+                    break
+urls.close()
+file.close()
