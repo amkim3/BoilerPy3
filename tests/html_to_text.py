943f0646b1b0a56e1af92b887dbb18fba0c5dce1
amkim3 <amkim3@crimson.ua.edu>
2022-11-16 10:20:08 -0600

    remove tests

--- tests/html_to_text.py
+++
@@ -1,9 +0,0 @@
-from boilerpy3 import extractors
-
-
-def html_to_text(html_file):
-    extractor = extractors.KeepEverythingExtractor()
-
-    with open("htmlExtractedText.txt", "a") as file:
-        file.write(extractor.get_content_from_file(html_file))
-    file.close()
