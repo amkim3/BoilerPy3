24937932cd96054dd024d19fc3617a88279f0a84
amkim3 <amkim3@crimson.ua.edu>
2022-11-17 04:12:35 -0600

    Spacy output

--- research/spacytraining.py
+++ research/spacytraining.py
@@ -60,18 +60,22 @@ def main():
     #     "HOURS": ["Monday - Friday  8:00 A.M. - 2:00 P.M.", "Monday - Friday 9:00 A.M. - 1:00 P.M."]
     # }
     dic = {
-        "LOC": ["Bibb co.", "Fayette Co.", "Greene Co.", "Hale Co.", "Lamar Co.", "Pickens Co.", "Tuscaloosa Co."],
-        "ORG": ["Bibb Co. Farmers Market", "Fayette Co. Farmers Market", "Greene Co. Farmers Market", ]
-        "ADDRESS": ["10445 AL-5 Brent, AL 35034", "8515 Hwy. 5 Woodstock, AL 35188"],
-        "PHONE": ["205-926-3968", "205-938-9790"],
-        "PERSON": ["Wanda Holder", "Frankie Grammer"],
-        "EMAIL": ["sacbrent@hotmail.com"],
-        "HOURS": ["Monday - Friday  8:00 A.M. - 2:00 P.M.", "Monday - Friday 9:00 A.M. - 1:00 P.M."]
+        # "LOC": ["Bibb co.", "Fayette Co.", "Greene Co.", "Hale Co.", "Lamar Co.", "Pickens Co.", "Tuscaloosa Co."],
+        "LOC": ["Bibb co.", "Fayette Co.", "Greene Co."],
+        "ORG": ["Bibb Co. Farmers Market", "Fayette Co. Farmers Market", "Greene Co. Farmers Market", "United Farmers Market"],
+        "ADDRESS": ["268 Belcher St Centreville, AL 35042", "650 McConnell Loop Fayette, AL", "137 Furse Ave Eutaw, AL 35462", "Co Rd 18 & Co Rd 41 Forkland, AL"],
+        "PHONE": ["334-518-9049", "205-596-3904", "205-372-9458", "205-799-5204", "334-289-4645"],
+        "HOURS": ["Saturdays May - Oct. 8:00 A.M. - 12:00 P.M.", "June Tues. & Saturdays 7:00 A.M. - Until Thursdays 4:00 P.M. - 7:00 P.M.", "Saturdays April - Sept. 8:00 A.M. - 12:00 P.M.", "Saturdays May - Oct. 10:00 A.M. - 3:00 P.M."]
     }
     # text1 = "Brent Senior Activity Center 10445 AL-5 Brent, AL 35034 205-926-3968 Center Director:  Wanda Holder sacbrent@hotmail.com Open Monday - Friday  8:00 A.M. - 2:00 P.M."
     # text2 = "Woodstock Senior Activity Center 28515 Hwy. 5 Woodstock, AL 35188 205-938-9790 Center Director:  Frankie Grammer Open Monday - Friday 9:00 A.M. - 1:00 P.M."
+    with open("Alabama_Senior_Farmers_Market_Nutrition_Program__FMNP____Area_Agency_on_Aging_of_West_Alabama.txt", "r") as f:
+        text = f.read()
+        structure_training_data(text,dic)
+    f.close()
     # structure_training_data(text1, dic)
     # structure_training_data(text2, dic)
+
     print(collective_dict)
 
 
